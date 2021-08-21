44e32817aff3b3e9fc0428b7f332c6e74fa974ca
Arie Trouw <arie.trouw@xy.compay>
2021-08-21 15:13:13 -0700

    depcheck error trap?

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 import { execFileSync } from 'child_process'
 console.log(`Deps [${process.cwd()}]`)
-execFileSync('yarn', ['depcheck'], { stdio: 'inherit' })
+try {
+  execFileSync('yarn', ['depcheck'], { stdio: 'inherit' })
+  process.exit(0)
+} catch (ex) {
+  console.log('Error')
+  process.exit(1)
+}
