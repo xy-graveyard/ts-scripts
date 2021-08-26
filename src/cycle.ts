6917554905f9e0838e1779e3ee521b02f741428d
Arie Trouw <arie.trouw@xy.compay>
2021-08-26 16:52:53 -0700

    fixed cycle script

--- src/cycle.ts
+++ src/cycle.ts
@@ -3,6 +3,6 @@ import { execFileSync } from 'child_process'
 console.log(`Cycle [${process.cwd()}]`)
 execFileSync(
   'yarn',
-  ['eslint', '--ext', '.js,.jsx,.ts,.tsx,.json', '--rule', "'import/no-cycle: [1, { maxDepth: 10 }]'", './src'],
+  ['eslint', '--ext', '.js,.jsx,.ts,.tsx,.json', '--rule', "{'import/no-cycle: [1, { maxDepth: 10 }]'}", './src'],
   { stdio: 'inherit' }
 )
