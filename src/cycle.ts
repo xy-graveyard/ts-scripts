d0b8de7f8e8519fa3672ccc70f3722c11648fc78
Arie Trouw <arie.trouw@xylabs.com>
2021-11-24 07:32:28 -0800

    Readme and cycle config

--- src/cycle.ts
+++ src/cycle.ts
@@ -4,9 +4,7 @@ import { execFileSync } from 'child_process'
 import { safeExit } from './safeExit'
 safeExit(() => {
   console.log(`Cycle [${process.cwd()}]`)
-  execFileSync(
-    'yarn',
-    ['eslint', '--ext', '.js,.jsx,.ts,.tsx,.json', '--rule', "{'import/no-cycle': [1, { maxDepth: 10 }]}", './src'],
-    { stdio: 'inherit' }
-  )
+  execFileSync('yarn', ['eslint', '-c', 'node_modules/@xylabs/ts-scripts/cycle.eslintrc', './src'], {
+    stdio: 'inherit',
+  })
 })
--- src/deps.ts
+++ src/deps.ts
@@ -4,11 +4,5 @@ import { execFileSync } from 'child_process'
 import { safeExit } from './safeExit'
 safeExit(() => {
   console.log(`Deps [${process.cwd()}]`)
-  try {
-    execFileSync('yarn', ['depcheck'], { stdio: 'inherit' })
-    process.exit(0)
-  } catch (ex) {
-    console.log('Error')
-    process.exit(1)
-  }
+  execFileSync('yarn', ['depcheck'], { stdio: 'inherit' })
 })
