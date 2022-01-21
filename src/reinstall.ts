93df4221bdf7b512fca3cb4a7b857b1d2ea35ecf
Joel Carter <joelbcarter@gmail.com>
2022-01-21 08:31:05 -0600

    `rimraf` instead of `rm -r -f`

--- src/reinstall.ts
+++ src/reinstall.ts
@@ -4,7 +4,6 @@ import { execFileSync } from 'child_process'
 import { safeExit } from './safeExit'
 safeExit(() => {
   console.log(`Reinstall [${process.cwd()}]`)
-  execFileSync('rm', ['-r', '-f', './node_modules'], { shell: true, stdio: 'inherit' })
-  execFileSync('rm', ['-f', './yarn.lock'], { shell: true, stdio: 'inherit' })
+  execFileSync('yarn', ['rimraf', './node_modules'], { shell: true, stdio: 'inherit' })
   execFileSync('yarn', ['install'], { shell: true, stdio: 'inherit' })
 })
