ef9974b51bd49f168991c95ffcff0a1cd1b6d02e
Arie Trouw <arie.trouw@xy.company>
2022-01-25 23:09:00 -0800

    fixed rimraf order in reinstall

--- src/reinstall.ts
+++ src/reinstall.ts
@@ -4,7 +4,7 @@ import { execSync } from 'child_process'
 import { safeExit } from './safeExit'
 safeExit(() => {
   console.log(`Reinstall [${process.cwd()}]`)
-  execSync('yarn rimraf ./node_modules', { stdio: 'inherit' })
   execSync('yarn rimraf ./yarn.lock', { stdio: 'inherit' })
+  execSync('yarn rimraf ./node_modules', { stdio: 'inherit' })
   execSync('yarn install', { stdio: 'inherit' })
 })
