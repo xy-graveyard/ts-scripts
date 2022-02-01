77cfc73fcb1fcaa0f57249fe862198a7b4802403
Arie Trouw <arie.trouw@xylabs.com>
2022-01-31 18:38:03 -0800

    copy images for compile

--- src/compile.ts
+++ src/compile.ts
@@ -4,7 +4,11 @@ import { execSync } from 'child_process'
 import { safeExit } from './safeExit'
 safeExit(() => {
   console.log(`Compile [${process.cwd()}]`)
+  const imageExtList = ['gif', 'png', 'svg', 'jpg', 'jpeg', 'webp']
   execSync('yarn rimraf dist', { stdio: 'inherit' })
   execSync('yarn rollup -c', { stdio: 'inherit' })
   execSync('yarn tsc -p tsconfig.build.json', { stdio: 'inherit' })
+  imageExtList.forEach((ext) => {
+    execSync(`yarn copyfiles -u 1 "./src/**/*.${ext}" dist`, { stdio: 'inherit' })
+  })
 })
