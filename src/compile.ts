858ab8b65512384d11f22e74abb2f66496091b56
Arie Trouw <arie.trouw@xylabs.com>
2022-05-02 12:04:12 -0700

    dlx for rollup

--- src/compile.ts
+++ src/compile.ts
@@ -6,8 +6,8 @@ safeExit(() => {
   console.log(`Compile [${process.cwd()}]`)
   const imageExtList = ['gif', 'png', 'svg', 'jpg', 'jpeg', 'webp']
   execSync('yarn dlx rimraf dist', { stdio: 'inherit' })
-  execSync('yarn rollup -c rollup.config.js', { stdio: 'inherit' })
-  execSync('yarn tsc -p tsconfig.build.json', { stdio: 'inherit' })
+  execSync('yarn dlx rollup -c rollup.config.js', { stdio: 'inherit' })
+  execSync('yarn dlx tsc -p tsconfig.build.json', { stdio: 'inherit' })
   imageExtList.forEach((ext) => {
     execSync(`yarn dlx copyfiles -u 1 "./src/**/*.${ext}" dist`, { stdio: 'inherit' })
   })
