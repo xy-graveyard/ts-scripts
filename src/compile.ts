95d3c3d24d3d4ecde1b78b258c7c8f2c75ae7279
Arie Trouw <arie.trouw@xylabs.com>
2022-05-06 12:53:14 -0700

    Auto stash before merge of "beta" and "origin/beta"
    quiet dlx

--- src/compile.ts
+++ src/compile.ts
@@ -5,10 +5,10 @@ import { safeExit } from './safeExit'
 safeExit(() => {
   console.log(`Compile [${process.cwd()}]`)
   const imageExtList = ['gif', 'png', 'svg', 'jpg', 'jpeg', 'webp']
-  execSync('yarn dlx rimraf dist', { stdio: 'inherit' })
-  execSync('yarn dlx rollup -c rollup.config.js', { stdio: 'inherit' })
+  execSync('yarn dlx -q rimraf dist', { stdio: 'inherit' })
+  execSync('yarn dlx -q rollup -c rollup.config.js', { stdio: 'inherit' })
   execSync('yarn tsc -p tsconfig.build.json', { stdio: 'inherit' })
   imageExtList.forEach((ext) => {
-    execSync(`yarn dlx copyfiles -u 1 "./src/**/*.${ext}" dist`, { stdio: 'inherit' })
+    execSync(`yarn dlx -q copyfiles -u 1 "./src/**/*.${ext}" dist`, { stdio: 'inherit' })
   })
 })
--- src/deps.ts
+++ src/deps.ts
@@ -4,5 +4,5 @@ import { execSync } from 'child_process'
 import { safeExit } from './safeExit'
 safeExit(() => {
   console.log(`Deps [${process.cwd()}]`)
-  execSync('yarn dlx depcheck', { stdio: 'inherit' })
+  execSync('yarn dlx -q depcheck', { stdio: 'inherit' })
 })
